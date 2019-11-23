04a25ed0b238fec08e563db9b47157e8520e664c
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-11-23 10:09:57 -0700

    added hotmoduoel reloading to index

--- bin/generators/appIndex.js
+++ bin/generators/appIndex.js
@@ -16,7 +16,10 @@ module.exports = async () => {
   const outputFile = `${projectRootDirectory}/src/_index.js`
   let masterString = `import React from 'react'
 import ReactDOM from 'react-dom'
+import { hot } from 'react-hot-loader/root'
 import * as OfflinePluginRuntime from 'offline-plugin/runtime'
+import { catchAddToHomeScreenPrompt } from '@tenjojeremy/web-toolkit/build/Misc-Utils/AddToHomeScreen/addToHomeScreen.index'
+
 
 ${importsString}
 import Router from './pages/router'
@@ -24,9 +27,12 @@ import Store from './store/_store'
 
 OfflinePluginRuntime.install()
 
-const App = () => (
+const App = hot(() => (
   ${compString}
-)
+))
+
+// Load asynchronous
+catchAddToHomeScreenPrompt()
 
 ReactDOM.render(<App />, document.getElementById('root'))
 `
--- src/_index.js
+++ src/_index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { hot } from 'react-hot-loader/root'
 import * as OfflinePluginRuntime from 'offline-plugin/runtime'
+import { catchAddToHomeScreenPrompt } from '@tenjojeremy/web-toolkit/build/Misc-Utils/AddToHomeScreen/addToHomeScreen.index'
+
 
 import './firebase/_firebase'
 
@@ -9,10 +12,13 @@ import Store from './store/_store'
 
 OfflinePluginRuntime.install()
 
-const App = () => (
+const App = hot(() => (
   <Store>
     <Router />
   </Store>
-)
+))
+
+// Load asynchronous
+catchAddToHomeScreenPrompt()
 
 ReactDOM.render(<App />, document.getElementById('root'))
