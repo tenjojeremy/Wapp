2eba5f1487c850447f6180b6b5097bfca0831a23
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-11-23 09:30:12 -0700

    removed webapck conig form wapp maniuegst

--- .wapp.manifest.js
+++ .wapp.manifest.js
@@ -1,18 +1,6 @@
 const firebaseConfig = require(`./src/firebase/firebase.config`)
 
 module.exports = {
-  projectInfo: {
-    name: 'App',
-    shortName: 'App',
-    description: 'App',
-    colors: {
-      primary: '#6200ee',
-      background: '#6200ee'
-    },
-    logo: {
-      path: './defaults/images/logo/logo.png'
-    }
-  },
   authentication: 'firebase',
   firebase: {
     config: firebaseConfig
