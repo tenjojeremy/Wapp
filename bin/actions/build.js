6bcd22f86eb2c6faf736cc04f30441c4eefad9fd
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-11-24 16:35:11 -0700

    fixed build

--- bin/actions/build.js
+++ bin/actions/build.js
@@ -1,5 +1,9 @@
 const shell = require('shelljs')
 
+const wappRoot = require('../utils/getModulePath')
+
 module.exports = async () => {
-  shell.exec('yarn webpack-scripts build')
+  const wappWebpackManifest = `${wappRoot('')}.webpack.manifest.js`
+
+  shell.exec(`yarn webpack-scripts build ${wappWebpackManifest}`)
 }
