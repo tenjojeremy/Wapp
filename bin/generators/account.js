fcf5e2c7149b8ec9b203a359457458a2aa99e27e
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-12-29 10:30:04 -0500

    clean success message

--- bin/generators/account.js
+++ bin/generators/account.js
@@ -2,10 +2,9 @@ const { projectRoot } = require('../utils/getModulePath')
 const createFile = require('../utils/createFile')
 const { logSuccessMessage } = require('../utils/logMessage')
 
-const successMessage = `Account generated`
-
 module.exports = async ({ wappManifest: { authentication } }) => {
   if (authentication) {
+    const successMessage = `Account generated`
     const providerName = `AuthProvider`
     const authImport = `@tenjojeremy/web-toolkit/build/Authentication/UseAuth/useAuth.index.js`
     const outputFileProvider = projectRoot('src/user/auth.state.js')
--- bin/generators/firebase/pushNotifications.js
+++ bin/generators/firebase/pushNotifications.js
@@ -2,9 +2,8 @@ const { wappDir } = require('../../utils/getModulePath')
 const createFile = require('../../utils/createFile')
 const { logSuccessMessage } = require('../../utils/logMessage')
 
-const successMessage = 'Firebase Push Notifications generated'
-
 module.exports = async () => {
+  const successMessage = 'Firebase Push Notifications generated'
   const output = wappDir('firebase/notifications.state.js')
   const content = `
 export { NotificationsProvider } from '@tenjojeremy/web-toolkit/build/Engagement/Push-Notifications/client/Firebase/React/useNotifications.index'
