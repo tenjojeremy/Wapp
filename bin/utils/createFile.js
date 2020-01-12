bbb4247e1991e21ac26a0604b6a10e3f5ff1d2df
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-01-12 11:45:00 -0500

    pretty

--- bin/utils/createFile.js
+++ bin/utils/createFile.js
@@ -3,7 +3,9 @@ const prettier = require('prettier')
 const prettierConfig = require('@tenjojeremy/prettier-config')
 
 module.exports = (outputFile, cssString, nojs) => {
-  const formateed = nojs ? cssString : prettier.format(cssString, { ...prettierConfig, parser: 'babel' })
+  const formateed = nojs
+    ? cssString
+    : prettier.format(cssString, { ...prettierConfig, parser: 'babel' })
   fs.outputFileSync(outputFile, formateed, (err) => {
     if (err) throw err
   })
