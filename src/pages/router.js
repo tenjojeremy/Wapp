defb813fe3a1d1ee9740920e30502f920af5140f
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-01-14 08:58:08 -0500

    clean

--- src/pages/router.js
+++ src/pages/router.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from '@tenjojeremy/web-toolkit/navigation/router'
 
-import Core from '../templates/core/core.index'
+import Core from '../templates/core'
 
 import HomeRoutes from './home/routes'
 import ProfileRoutes from './profile/routes'
