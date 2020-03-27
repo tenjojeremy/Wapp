de0b866f6571cfc64f62f8a80a4cd9e60602b812
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-03-27 18:49:40 -0400

    animateOnSiteLoad: remove if set to false

--- bin/generators/animateOnSiteLoad.js
+++ bin/generators/animateOnSiteLoad.js
@@ -3,21 +3,22 @@ const createFile = require('../utils/createFile')
 const { logSuccessMessage } = require('../utils/logMessage')
 
 module.exports = async ({ wappManifest: { animateOnSiteLoad } }) => {
-  const animateOnSiteLoadDisabled = animateOnSiteLoad === undefined
-  const animationDuration = animateOnSiteLoad || 500
+  const animateOnSiteLoadSetToFalse = animateOnSiteLoad === false
+  const defaultAnimationDuration = 500
+  const animationDuration = animateOnSiteLoad || defaultAnimationDuration
   const createdSplashMessage = `animateOnSiteLoad created`
   const hideFunctionFileDir = wappDir('animateOnSiteLoad/animateOnSiteLoad.js')
   const hideFunctionFile = `
   export default () => {
    ${
-     animateOnSiteLoadDisabled
-       ? ''
-       : ` const root = document.querySelector('#root')
+     !animateOnSiteLoadSetToFalse
+       ? ` const root = document.querySelector('#root')
     if (root.animate) {
       root.animate([{ opacity: 0 }, { opacity: 1 }], {
         duration: ${animationDuration},
       })
     }`
+       : ''
    }  
   }
   `
