10b0cb37a5e7b081af6017cb32253bd66fb5d4a6
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-03-29 08:01:52 -0400

    typogroapohy default clean

--- bin/defaults/theme/typography.js
+++ bin/defaults/theme/typography.js
@@ -7,42 +7,37 @@ module.exports = {
   types: `
   .typography-h1 {
     font-family:  var(--font-primary);
-    font-style: normal;
+
     font-weight: 300;
     font-size: 96px;
     letter-spacing: -1.5px;
   }
   .typography-h2 {
     font-family:  var(--font-primary);
-    font-style: normal;
+
     font-weight: 300;
     font-size: 60px;
     letter-spacing: -0.5px;
   }
   .typography-h3 {
     font-family:  var(--font-primary);
-  font-style: normal;
-  font-weight: normal;
   font-size: 48px;
   }
   .typography-h4 {
     font-family:  var(--font-primary);
-    font-style: normal;
-    font-weight: normal;
+
     font-size: 34px;
     letter-spacing: 0.25px;
   }
   
   .typography-h5 {
     font-family:  var(--font-primary);
-    font-style: normal;
-    font-weight: normal;
+
     font-size: 24px;
   }
   
   .typography-h6 {
     font-family:  var(--font-primary);
-    font-style: normal;
     font-weight: 500;
     font-size: 20px;
     letter-spacing: 0.15px;
@@ -50,37 +45,29 @@ module.exports = {
   
   .typography-body1 {
     font-family:  var(--font-primary);
-    font-style: normal;
-    font-weight: normal;
     font-size: 18px;
     letter-spacing: 0.5px;
   }
   
   .typography-body2 {
     font-family:  var(--font-primary);
-  font-style: normal;
-  font-weight: normal;
   font-size: 14px;
   letter-spacing: 0.25px;
   }
   
   .typography-subtitle1 {
     font-family:  var(--font-primary);
-  font-style: normal;
-  font-weight: normal;
   font-size: 16px;
   letter-spacing: 0.15px;
   }
   
   .typography-subtitle2 {
     font-family:  var(--font-primary);
-    font-style: normal;
-    font-weight: normal;
-    font-size: 14px;
+    font-size: 18px;
   }
   .typography-button {
     font-family:  var(--font-primary);
-    font-style: normal;
+
     font-weight: bold;
     font-size: 14px;
     letter-spacing: 0.75px;
@@ -88,8 +75,6 @@ module.exports = {
   }
   .typography-caption {
     font-family:  var(--font-primary);
-  font-style: normal;
-  font-weight: normal;
   font-size: 12px;
   letter-spacing: 0.4px;
   }
@@ -100,7 +85,7 @@ module.exports = {
   }
   .typography-overline {
     font-family:  var(--font-primary);
-    font-style: normal;
+
     font-weight: 500;
     font-size: 10px;
     letter-spacing: 1.5px;
