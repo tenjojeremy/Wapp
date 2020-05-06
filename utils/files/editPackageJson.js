ce9a47af8af73aae1f0680c275c9bdca96f4b00b
jtenjo <jtenjo@criticalmass.com>
2020-05-06 17:46:02 -0400

    clen

--- utils/files/editPackageJson.js
+++ utils/files/editPackageJson.js
@@ -3,7 +3,6 @@ const { projectDir } = require('../getModulePath')
 
 module.exports = ({ method, value, isTest }) => {
   const file = editPackageJson(projectDir('package.json', isTest))
-  console.log({ method })
   file[method](value[0], value[1])
 
   file.save()
