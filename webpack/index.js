7723957274accbddea69e1323cca4e580b4c66e4
jtenjo <jtenjo@criticalmass.com>
2020-05-08 18:45:28 -0400

    fixed } error

--- webpack/index.js
+++ webpack/index.js
@@ -3,16 +3,15 @@ const { logSuccessMessage } = require('../utils/logMessage')
 const { projectDir, wappRootDir } = require('../utils/getModulePath')
 
 module.exports = ({ action, step = '2/2' }) => {
-  const envTest = process.env.TEST === 'true' ? '--env.test' : ''
   const configFilePath = wappRootDir('webpack/config.js')
   const wappManifestPath = wappRootDir('final.wapp.manifest.js')
   const actions = {
     start: {
-      command: `webpack-dev-server  --config ${configFilePath} --mode development --color --hot --inline --wappManifestPath ${wappManifestPath} ${envTest}}`,
+      command: `webpack-dev-server  --config ${configFilePath} --mode development --color --hot --inline --wappManifestPath ${wappManifestPath}`,
       log: 'Starting dev server...',
     },
     build: {
-      command: `webpack --config ${configFilePath} -p --wappManifestPath ${wappManifestPath} --color ${envTest}`,
+      command: `webpack --config ${configFilePath} -p --wappManifestPath ${wappManifestPath} --color`,
       log: 'Building webpack _build...',
     },
     analyze: {
